31ff248daee10c28f9051745571355a238a51fa7
Nicole Smith <nms5221@hotmail.com>
2016-09-15 09:38:22 -0700

    Create E11_Day_Of_Week_Unit_Tests.py

---
+++ E11_Day_Of_Week_Unit_Tests.py
@@ -0,0 +1,69 @@
+'''
+Unit Testing for Given Date, Get Day of Week
+https://www.reddit.com/r/dailyprogrammer/comments/pwons/2192012_challenge_11_easy/
+'''
+
+import unittest
+from E11_Day_Of_Week import get_day_of_week
+
+
+class test_day_of_week(unittest.TestCase):
+    def test_valid_date(self):
+        d = 5
+        m = 9
+        y = 2016
+        expected = 'Monday'
+        self.assertEquals(get_day_of_week(d, m, y), expected)
+
+    def test_invalid_day(self):
+        d = 31
+        m = 9
+        y = 2016
+        expected = 'Invalid Date'
+        self.assertEquals(get_day_of_week(d, m, y), expected)
+
+    def test_invalid_month(self):
+        d = 5
+        m = 13
+        y = 2016
+        expected = 'Invalid Date'
+        self.assertEquals(get_day_of_week(d, m, y), expected)
+
+    def test_invalid_year(self):
+        d = 5
+        m = 9
+        y = -1
+        expected = 'Invalid Date'
+        self.assertEquals(get_day_of_week(d, m, y), expected)
+
+    def test_invalid_float(self):
+        d = 5
+        m = 9
+        y = 2016.5
+        expected = 'Invalid Date'
+        self.assertEquals(get_day_of_week(d, m, y), expected)
+
+    def test_invalid_string(self):
+        d = 5
+        m = 'September'
+        y = 2016
+        expected = 'Invalid Date'
+        self.assertEquals(get_day_of_week(d, m, y), expected)
+
+    def test_valid_leap_year(self):
+        d = 29
+        m = 2
+        y = 2000
+        expected = 'Tuesday'
+        self.assertEquals(get_day_of_week(d, m, y), expected)
+
+    def test_invalid_leap_year(self):
+        d = 29
+        m = 2
+        y = 1999
+        expected = 'Invalid Date'
+        self.assertEquals(get_day_of_week(d, m, y), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
