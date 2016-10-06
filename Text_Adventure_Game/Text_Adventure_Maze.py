7ba66c5657d2c8524014565a6e5c2523fa963c11
Nicole Smith <nms5221@hotmail.com>
2016-10-06 07:46:37 -0700

    Update Text_Adventure_Maze.py

--- Text_Adventure_Game/Text_Adventure_Maze.py
+++ Text_Adventure_Game/Text_Adventure_Maze.py
@@ -3,16 +3,15 @@ Text Adventure Game
 https://www.reddit.com/r/dailyprogrammer/comments/pjbuj/intermediate_challenge_2/
 """
 
-import maze
 import pickle  # Allows us to read a dictionary from a file
-
+from Text_Adventure_Game import position
 
 if __name__ == "__main__":
     # Using the maze class and object
-    m = maze.Maze()
+    m = position.Maze()
 
     # Setting coordinates from a dictionary stored in a txt file
-    with open('coordinates.txt', 'rb') as handle:
+    with open('maze_coordinates.txt', 'rb') as handle:
         coordinates = pickle.loads(handle.read())
         m.set_coordinates(coordinates, coordinates.get('Entry'), coordinates.get('Exit'))
 
